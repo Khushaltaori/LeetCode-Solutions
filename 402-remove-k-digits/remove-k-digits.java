baa563621c640e5e0967579aa6fa83d79c5f08b7
Khushal Rajesh Taori <taorikr@rknec.edu>
2026-07-24 12:53:08 +0530

    Time: 26 ms (38.22%) | Memory: 48.1 MB (23.99%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,39 +1,38 @@
-import java.util.Stack;
-
 class Solution {
     public String removeKdigits(String num, int k) {
         Stack<Character> st = new Stack<>();
-        
-        for (char ch : num.toCharArray()) {
-            while (!st.isEmpty() && k > 0 && st.peek() > ch) {
+        for(char ch:num.toCharArray()){
+            while(!st.isEmpty() && k>0 && ch<st.peek()){
                 st.pop();
                 k--;
             }
+
             st.push(ch);
         }
+            while(!st.isEmpty() && k>0){
+                st.pop();
+                k--;
+            }
 
-        while (!st.isEmpty() && k > 0) {
-            st.pop();
-            k--;
-        }
+            
+            StringBuilder ans = new StringBuilder();
+            //trailing zeroes
+            while(!st.isEmpty()){
+                ans.append(st.pop());
+            }
 
-        StringBuilder ans = new StringBuilder();
-        // Pop everything until the stack is empty
-        while (!st.isEmpty()) {
-            ans.append(st.pop());
+            ans.reverse();
+
+            while(ans.length()>0 && ans.charAt(0)=='0'){
+                ans.deleteCharAt(0);
+            }
+
+            if(ans.length()==0){
+                return "0";
+            }else{
+                return ans.toString();
+            }
+
         }
         
-        ans.reverse();
-
-        // Strip leading zeros
-        while (ans.length() > 0 && ans.charAt(0) == '0') {
-            ans.deleteCharAt(0);
-        }
-
-        if (ans.length() == 0) {
-            return "0";
-        } else {
-            return ans.toString();
-        }
-    }
 }
