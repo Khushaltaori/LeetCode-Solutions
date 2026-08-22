62915e60eb5c88beffa272f939d9eadda7488c55
Khushal Rajesh Taori <taorikr@rknec.edu>
2026-08-22 13:38:45 +0530

    Time: 1 ms (100.00%) | Memory: 46.1 MB (98.59%) - LeetSync

---
+++ 4378-nearest-available-drone/nearest-available-drone.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int nearestDrone(int[][] drones, int[] target) {
+        int n = drones.length;
+        int ans = -1;
+        int minDistance = Integer.MAX_VALUE;
+
+        for(int i=0;i<n;i++){
+            int x = drones[i][0];
+            int y = drones[i][1];
+            int range = drones[i][2];
+
+            int distance = Math.abs(x-target[0]) + Math.abs(y-target[1]);
+
+            if(distance <= range && distance < minDistance){
+                minDistance = distance;
+                ans = i;
+            }
+        }
+        return ans;
+    }
+}
