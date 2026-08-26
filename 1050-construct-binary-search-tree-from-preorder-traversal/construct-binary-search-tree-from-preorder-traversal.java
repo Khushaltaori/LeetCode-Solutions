252da1df5e4fe03d86bc42a078806b6beebfabc0
Khushal Rajesh Taori <taorikr@rknec.edu>
2026-08-26 12:51:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (88.14%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -1,28 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
     public TreeNode bstFromPreorder(int[] preorder) {
+        
         int[] index = {0};
-
-        return build(preorder, Integer.MAX_VALUE, index);
+        return build(preorder,Integer.MAX_VALUE,index);
     }
 
-    private TreeNode build(int[] preorder, int bound, int[] index) {
+    public TreeNode build(int[] preorder,int bound,int[] index){
+        if(index[0]==preorder.length || preorder[index[0]] > bound){
 
-        // No elements left OR current value cannot be placed here
-        if (index[0] == preorder.length || preorder[index[0]] > bound) {
             return null;
         }
 
-        // Create root using current preorder element
         TreeNode root = new TreeNode(preorder[index[0]++]);
-
-        // Build left subtree
-        // All values must be smaller than root.val
-        root.left = build(preorder, root.val, index);
-
-        // Build right subtree
-        // Values can go up to the current bound
-        root.right = build(preorder, bound, index);
+        root.left = build(preorder,root.val,index);
+        root.right = build(preorder,bound,index);
 
         return root;
     }
