cc642ee0d1a4a9df07356234a27603ba68e0e9e4
Khushal Rajesh Taori <taorikr@rknec.edu>
2026-08-26 13:41:33 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (15.44%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        int n = s.length();
+        int left = 0;
+        int right = 0;
+        int countones = 0;
+        String ans = "";
+        while(right<n){
+            if(s.charAt(right)=='1' ){
+                countones++;
+            }
+            right++; 
+
+            while(countones>k){
+                if(s.charAt(left)=='1'){
+                countones--;
+                }
+             left++;
+            }
+
+            if(countones == k){
+                while(s.charAt(left) =='0'){
+                    left++;
+                }
+
+                String current = s.substring(left,right);
+
+                                if (ans.equals("") ||
+
+                    current.length() < ans.length() ||
+
+                    (current.length() == ans.length() &&
+
+                     current.compareTo(ans) < 0)) {
+
+                    ans = current;
+
+                }
+            }
+            
+            
+           
+        }
+        return ans;
+    }
+
+    
+}
