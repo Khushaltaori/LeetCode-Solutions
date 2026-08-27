1dda821d0a9b71dc4e1b7f4c72b0815c1b3d5a70
Khushal Rajesh Taori <taorikr@rknec.edu>
2026-08-27 19:26:48 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (95.27%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,48 +1,37 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        int n = s.length();
-        int left = 0;
-        int right = 0;
-        int countones = 0;
-        String ans = "";
-        while(right<n){
-            if(s.charAt(right)=='1' ){
-                countones++;
-            }
-            right++; 
-
-            while(countones>k){
-                if(s.charAt(left)=='1'){
-                countones--;
-                }
-             left++;
-            }
-
-            if(countones == k){
-                while(s.charAt(left) =='0'){
-                    left++;
-                }
-
-                String current = s.substring(left,right);
-
-                                if (ans.equals("") ||
-
-                    current.length() < ans.length() ||
-
-                    (current.length() == ans.length() &&
-
-                     current.compareTo(ans) < 0)) {
-
-                    ans = current;
-
-                }
-            }
-            
-            
-           
+     int n = s.length();
+     int left =0;
+     int right = 0;
+     int countones = 0;
+    String ans = "";
+     while(right<n){
+        if(s.charAt(right)=='1'){
+            countones++;
         }
-        return ans;
-    }
+        right++;
 
-    
+        while(countones>k){
+            if(s.charAt(left)=='1'){
+                countones--;
+            }
+            left++;
+        }
+
+        if(countones == k){
+            while(s.charAt(left)=='0'){
+                left++;
+            }
+
+            String curr = s.substring(left,right);
+
+            if(ans.equals("")|| curr.length()<ans.length() || curr.length()==ans.length() && curr.compareTo(ans)<0){
+            ans = curr;
+            }
+        }
+
+        
+     }   
+     return ans;
+    }
 }
