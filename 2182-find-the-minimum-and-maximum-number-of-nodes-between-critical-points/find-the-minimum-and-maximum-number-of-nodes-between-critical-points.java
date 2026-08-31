57c235aa1fdab12f0828edfac52aa37e4bff9f86
Khushal Rajesh Taori <taorikr@rknec.edu>
2026-08-31 14:49:28 +0530

    Time: 4 ms (99.87%) | Memory: 106.1 MB (55.32%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -1,9 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
+        
+        int[] ans = {-1,-1};
 
-        int[] ans = {-1, -1};
-
-        if (head == null || head.next == null || head.next.next == null) {
+        if(head == null || head.next == null || head.next.next == null){
             return ans;
         }
 
@@ -12,44 +22,33 @@ class Solution {
 
         int first = -1;
         int last = -1;
-        int minDist = Integer.MAX_VALUE;
+        int index=1;
+        int minDis = Integer.MAX_VALUE;
 
-        int index = 1;
-
-        while (temp.next != null) {
-
+        while(temp.next!=null){
             ListNode next = temp.next;
 
-            // Check for critical point
-            if ((temp.val > prev.val && temp.val > next.val) ||
-                (temp.val < prev.val && temp.val < next.val)) {
-
-                if (first == -1) {
-                    // First critical point
+            if((temp.val > prev.val && temp.val > next.val) || (temp.val< prev.val && temp.val < next.val)){
+                if(first == -1){
                     first = index;
-                } else {
-                    // Distance from previous critical point
-                    minDist = Math.min(minDist, index - last);
+                }else{
+                    minDis = Math.min(minDis,index - last);
                 }
 
-                // Current critical point becomes last
                 last = index;
             }
 
-            // Move forward
             prev = temp;
             temp = next;
             index++;
         }
 
-        // Fewer than 2 critical points
-        if (first == -1 || first == last) {
+        if(first == -1 || first == last){
             return ans;
         }
 
         int maxDist = last - first;
-
-        ans[0] = minDist;
+        ans[0] = minDis;
         ans[1] = maxDist;
 
         return ans;
