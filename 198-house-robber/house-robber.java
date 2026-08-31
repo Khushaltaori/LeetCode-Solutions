66b87d1cde75dac138e3e948c22843b790349ae5
Khushal Rajesh Taori <taorikr@rknec.edu>
2026-09-01 00:52:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (5.68%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,20 +1,19 @@
 class Solution {
-
-    public int func(int ind, int[] nums, int[] dp){
-
-        if(ind == 0) return nums[ind];
-        if(ind < 0) return 0;
-
-        if(dp[ind]!=-1) return dp[ind];
-        int pick = nums[ind] + func(ind -2,nums,dp);
-        int notpick = 0 + func(ind - 1,nums,dp);
-
-        return dp[ind] = Math.max(pick,notpick);
-    }
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return func(n-1,nums,dp);
+        int prev = nums[0];
+        int prev2 = 0;
+
+        for(int i=0;i<n;i++){
+            int take = nums[i];
+            if(i>1) take += prev2;
+
+            int nontake = 0 + prev;
+
+            int curi = Math.max(take,nontake);
+            prev2 = prev;
+            prev = curi;
+        }
+        return prev;
     }
 }
