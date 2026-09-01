0dd9b5180b421fb5c8abc10bc56e6bf121a47c7a
Khushal Rajesh Taori <taorikr@rknec.edu>
2026-09-01 18:42:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (37.90%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,7 +1,7 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int prev = nums[0];
+        int prev = 0;
         int prev2 = 0;
 
         for(int i=0;i<n;i++){
