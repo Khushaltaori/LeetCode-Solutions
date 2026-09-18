968ef7245238f6fb9b137bb70872add12aabe948
Khushal Rajesh Taori <taorikr@rknec.edu>
2026-09-19 00:08:21 +0530

    Time: 1 ms (13.39%) | Memory: 42.1 MB (65.20%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,26 +1,37 @@
 class Solution {
 
-    public int func(int i ,int j,int[][] dp){
-        
-        
-       
-        if(i == 0 && j == 0) return 1;
+    public int func(int m, int n, int[][] dp) {
 
-        if(i<0 || j<0) return 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
 
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up = func(i-1,j,dp);
-        int left = func(i,j-1,dp);
+                // Base case
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } 
+                else {
+                    int up = 0;
+                    int left = 0;
 
-        return dp[i][j] = up+left;
+                    if (i > 0) {
+                        up = dp[i - 1][j];
+                    }
+
+                    if (j > 0) {
+                        left = dp[i][j - 1];
+                    }
+
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
+
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
 
-         for(int[] row : dp){
-            Arrays.fill(row,-1);
-        }
-
-        return func(m-1,n-1,dp);
+        return func(m, n, dp);
     }
 }
